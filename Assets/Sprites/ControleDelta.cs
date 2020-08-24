70bf87711ef3f1c586b6c69e569e77944af78e9d
CvgonBR01 <69465776+CvgonBR01@users.noreply.github.com>
2020-08-23 23:18:36 -0300

    Add files via upload

---
+++ Assets/Sprites/ControleDelta.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleDelta : MonoBehaviour
+{
+    Animator anim;// Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("Movendo", true);
+
+        }
+        else
+        {
+            anim.SetBool("Movendo", false);
+            
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Pulando", true);
+        }
+        else
+        {
+            anim.SetBool("Pulando", false);
+
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            anim.SetBool("Atirando", true);
+        }
+        else
+        {
+            anim.SetBool("Atirando", false);
+
+        }
+    }
+}
