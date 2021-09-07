14155714498a5171718e8b416531bf15d490f8b0
benjunki <benja02jun@gmail.com>
2021-09-07 16:38:50 -0300

    Melhora no ia do inimigo

--- Assets/Scripts/EnemyGFX.cs
+++ Assets/Scripts/EnemyGFX.cs
@@ -11,11 +11,11 @@ public class EnemyGFX : MonoBehaviour
     {
         if(aiPath.desiredVelocity.x>= 0.01f)
         {
-            transform.localScale=new Vector3(-1f,1f,1f);
+            transform.localScale=new Vector3(1f,1f,1f);
         }    
         else if(aiPath.desiredVelocity.x<=-0.01f)
         {
-            transform.localScale=new Vector3(1f,1f,1f);
+            transform.localScale=new Vector3(-1f,1f,1f);
         }
     }
 }
