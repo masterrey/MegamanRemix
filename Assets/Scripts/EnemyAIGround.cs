7fe66e8b49281a0b1b852a565fc0fd9bd94fbc96
benjunki <benja02jun@gmail.com>
2021-09-13 08:42:09 -0300

    IA de tiro

---
+++ Assets/Scripts/EnemyAIGround.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAIGround : MonoBehaviour
+{
+    [Header("Pathfinding")]
+    public Transform target;
+    public float activateDistance = 50f;
+    public float pathUpdateSeconds = 0.5f;
+
+    [Header("Physics")]
+    public Transform enemyGFX;
+    public float speed = 200f;
+    public float nextWaypointDistance = 3f;
+    public float jumpNodeHeightRequirement = 0.8f;
+    public float jumpModifier = 0.3f;
+    public float jumpCheckOffset = 0.1f;
+
+    [Header("Custom Behavior")]
+    public bool followEnabled = true;
+    public bool jumpEnabled = true;
+    public bool directionLookEnabled = true;
+
+    private Path path;
+    int currentWaypoint = 0;
+    bool isGrounded = false;
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, pathUpdateSeconds);
+    }
+    
+    void FixedUpdate() 
+    {
+        if(TargetInDistance() && followEnabled)
+        {
+            PathFollow();
+        }
+    }
+
+    void UpdatePath() 
+    {
+        if(followEnabled && TargetInDistance() && seeker.IsDone())  
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }  
+    }
+
+    void PathFollow()
+    {
+        if(path==null)
+        {
+            return;
+        }
+        if(currentWaypoint>=path.vectorPath.Count)
+        {
+            return;
+        }
+
+        isGrounded = Physics2D.Raycast(transform.position, -Vector3.up, GetComponent<Collider2D>().bounds.extents.y + jumpCheckOffset);
+
+        //calculo de direção
+        Vector2 direction= ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction*speed*Time.deltaTime;
+
+        //Pulo
+        if (jumpEnabled && isGrounded)
+        {
+            if(direction.y>jumpNodeHeightRequirement)
+            {
+                rb.AddForce(Vector2.up*speed*jumpModifier);
+            }
+        }
+
+        //Movimento
+        rb.AddForce(force);
+
+        //next waypoint
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+        if(distance<nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+
+        //Direção do sprite
+        if(directionLookEnabled)
+        {
+            if(force.x>=0.01f)
+            {
+                enemyGFX.localScale = new Vector3(1f,1f,1f);
+            }
+            else if(force.x<=0.01f)
+            {
+                enemyGFX.localScale = new Vector3(-1f,1f,1f);
+            }
+        }
+    }
+
+    private bool TargetInDistance()
+    {
+        return Vector2.Distance(transform.position, target.transform.position)<activateDistance;
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if(!p.error)
+        {
+            path = p;
+            currentWaypoint=0;
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyAIShoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAIShoot : MonoBehaviour
+{   
+    public Transform player; 
+    public float range;
+    public GameObject projectile;
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    void Start() 
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwShots=startTimeBtwShots; 
+    }
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, player.position) < range)
+        {
+            print("Chegou");
+        }
+        if(timeBtwShots<=0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots=startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
