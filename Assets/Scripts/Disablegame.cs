13a06871239ae2f6c2ebba2f0e3f3e78c61c6899
cavernadormaster <89394365+cavernadormaster@users.noreply.github.com>
2021-09-20 10:23:28 -0300

    Add files via upload

---
+++ Assets/Scripts/Disablegame.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disablegame : MonoBehaviour
+{
+    public GameObject square;
+
+    void Start()
+    {
+        if (square.activeInHierarchy == true)
+        {
+            square.SetActive(false);
+        }
+    }
+      
+    void Update()
+    {
+        if (NinjaComando.active == true)
+        {
+            if (square.activeInHierarchy == false)
+                square.SetActive(true);
+        
+        }
+        else if(NinjaComando.active == false)
+        {
+            if(square.activeInHierarchy == true)
+            {
+                square.SetActive(false);
+
+            }
+
+        }
+         
+    }
+    
+}
---
+++ Assets/Scripts/Disablegame2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disablegame2 : MonoBehaviour
+{
+    public GameObject square2;
+
+    void Start()
+    {
+        if (square2.activeInHierarchy == true)
+        {
+            square2.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (NinjaComando.active2 == true)
+        {
+            if (square2.activeInHierarchy == false)
+                square2.SetActive(true);
+
+        }
+        else if (NinjaComando.active2 == false)
+        {
+            if (square2.activeInHierarchy == true)
+            {
+                square2.SetActive(false);
+
+            }
+
+        }
+
+    }
+}
