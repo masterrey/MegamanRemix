f5b49f6e409f12e4c7f321ae1f15ab1561dc486a
cavernadormaster <89394365+cavernadormaster@users.noreply.github.com>
2021-09-21 13:49:59 -0300

    Add files via upload

---
+++ Assets/Scripts/EnemyFollowPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowPlayer : MonoBehaviour
+{
+    public float speed;
+    public float lineOfSight;
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+
+
+    void Update()
+    {
+        float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
+        if (distanceFromPlayer < lineOfSight)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
+        }
+
+        if (player.position.x <= transform.position.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (player.position.x >= transform.position.x)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, lineOfSight);
+    }
+}
+
