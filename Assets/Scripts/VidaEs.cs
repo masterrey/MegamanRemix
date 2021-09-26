1b309a116d14e72f370f01f0655f3b97f716eaed
benjunki <benja02jun@gmail.com>
2021-09-26 01:16:01 -0300

    Fase de Neve

---
+++ Assets/Scripts/VidaEs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VidaEs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int Vidas=1;
+    
+    private void OnCollisionEnter2D(Collision2D collision) //Morte
+    {
+        if(collision.gameObject.tag=="TirarVida") 
+        {
+            Vidas--;//vidas-1= ou vidas=vidas-1;
+            if(Vidas==0)
+            {
+                Die();
+            }
+        }
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
